58c1dec70b37295f2b021a7d2fb40ac1babf6c4f
shivami-shah <shivamishah@gmail.com>
2025-06-28 13:27:02 +0530

    update configurations

--- app.py
+++ app.py
@@ -1,21 +1,4 @@
-import os
-from excel_data_extractor import process_files_in_directory
-import ft_training_data_generator_exp1 as exp1
-import ft_training_data_generator_exp2 as exp2
-import ft_training_data_generator_exp3 as exp3
+
 
 if __name__ == "__main__":
-    
-    extract_data_from_excel_files = True
-    
-    if extract_data_from_excel_files:
-        daily_trackers_folder = 'data/daily_trackers'
-        titles_and_urls_folder = 'data/titles_and_urls'
-        process_files_in_directory(daily_trackers_folder, titles_and_urls_folder)
-        
-    titles_and_urls_folder = os.path.join('data', 'titles_and_urls')
-    output_finetuning_dir = os.path.join('data', 'finetuning_data')
-    
-    exp1.generate_finetuning_data_for_directory(titles_and_urls_folder, output_finetuning_dir)
-    exp2.generate_finetuning_data_for_directory(titles_and_urls_folder, output_finetuning_dir)
-    exp3.generate_finetuning_data_for_directory(titles_and_urls_folder, output_finetuning_dir)
+    pass
---
+++ config.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+api_key=os.getenv("OPENAI_API_KEY")
+PROJECT_ROOT = Path(__file__).resolve().parent
+
+DATA_DIR = PROJECT_ROOT / "data"
+RAW_DATA_DIR = DATA_DIR / "daily_trackers"
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
+DATA_FOR_FT_DIR = DATA_DIR / "data_for_ft"
+PROMPTS_DIR = PROJECT_ROOT / "prompts"
+MODEL_TESTING_OUTPUT_DIR = DATA_DIR / "model_testing_output"
+LOGS_DIR = PROJECT_ROOT / "logs"
+
+# Ensure directories exist (optional, but good practice for project setup)
+DATA_DIR.mkdir(exist_ok=True)
+RAW_DATA_DIR.mkdir(parents=True, exist_ok=True)
+PROCESSED_DATA_DIR.mkdir(parents=True, exist_ok=True)
+LOGS_DIR.mkdir(exist_ok=True)
+DATA_FOR_FT_DIR.mkdir(parents=True, exist_ok=True)
+MODEL_TESTING_OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+PROMPTS_DIR.mkdir(parents=True, exist_ok=True)
